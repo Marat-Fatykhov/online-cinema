7a916a4c46a7dafab3d97e9cb0a2fea50fb4547d
Marat-Fatykhov <79593643+Marat-Fatykhov@users.noreply.github.com>
2024-02-22 21:39:22 +0300

    Update apis.js

--- src/apis.js
+++ src/apis.js
@@ -1,5 +1,5 @@
 const account_id = 20967720;
-
+// вынести все переменные в env файл
 export const APIS = {
     API_AUTHORIZATION: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmRkYmJhNjdjYzIzOGYxYjlkYTc2ZjFmZTlkMTAyZSIsInN1YiI6IjY1YmZkYmRmNDM5OTliMDE4NGM4NThjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TDTqMYy2qJP5gEkLFc-ni0OOr9C1EWRQNcgdJ508KC0',
     API_GENRES: 'https://api.themoviedb.org/3/genre/movie/list',
